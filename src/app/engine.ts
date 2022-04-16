65ef69e6f06c8c5bcfee6020dbbaae422e4fb8a4
waynemorphic <wayneklyde@gmail.com>
2022-04-16 16:20:31 +0300

    generated blueprint class engine and added public properties

---
+++ src/app/engine.ts
@@ -0,0 +1,9 @@
+export class Engine {
+    constructor(
+        public name: string,
+        public repo: string,
+    ){}
+}
+
+//Engine blueprint class that contains the properties of what will be contained
+// in the form for searching up user names and specific repositories
