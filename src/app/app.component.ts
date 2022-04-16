a20eabebbc4b17e445b2d63de239a91afc3629c9
waynemorphic <wayneklyde@gmail.com>
2022-04-16 16:22:18 +0300

    class and component commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Engine } from './engine';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'github-search';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchEngineComponent } from './search-engine/search-engine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchEngineComponent
   ],
   imports: [
     BrowserModule,
