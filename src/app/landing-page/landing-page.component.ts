58918c5fccb70595160b88e87267dfd2253474c1
waynemorphic <wayneklyde@gmail.com>
2022-04-17 20:08:41 +0300

    added event emitters and defined methods for search buttons of both the users and the repositories

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Users } from '../users'; //imported class Users
+import { Repositories } from '../repositories'; //imported class repositories
 
 @Component({
   selector: 'app-landing-page',
@@ -6,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+
+  newUsers = new Users ("", 0, 0, 0, new Date(), new Date())//constructing newUsers based on the Users class
+
+  newRepositories = new Repositories ("","", 0, 0) //constructing the repositories based on Repositories class
+
+  @Output() addUser = new EventEmitter<Users>(); //we want to emit the event clicked from parent for searching a user
+
+  @Output() addRepository = new EventEmitter<Repositories>(); //emitting the repositories from the parent for searching the repo
+
+  //defining method for searching a username once search button is clicked
+  searchUsername(){
+    this.addUser.emit(this.newUsers);
+  }
+
+  //defining method for searching a repository once the search button is clicked
+  searchRepo(){
+    this.addRepository.emit(this.newRepositories);
+  }
 
   constructor() { }
 
