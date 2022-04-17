93cd3921fbcaf101d158d9ed675fc83fa318a960
waynemorphic <wayneklyde@gmail.com>
2022-04-17 19:47:49 +0300

    created public properties for class users

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,2 +1,10 @@
 export class Users {
+    constructor(
+        public name: string,
+        public followers: number,
+        public following: number,
+        public Repositories: number,
+        public createdAt: Date,
+        public updatedAt: Date
+    ){}
 }
