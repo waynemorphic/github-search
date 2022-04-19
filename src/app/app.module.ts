7d10daeee6214e97df0bb28502db07fbcba8fa15
waynemorphic <wayneklyde@gmail.com>
2022-04-19 15:13:34 +0300

    changes to the files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; //imported forms module to manipulate the NgModule
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { GithubUsersSearchComponent } from './github-users-search/github-users-search.component';
+import { GithubRepositoriesSearchComponent } from './github-repositories-search/github-repositories-search.component';
+import { GithubUsersServiceService } from './github-users-service.service';
+import { UserResultComponent } from './user-result/user-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
+    GithubUsersSearchComponent,
+    GithubRepositoriesSearchComponent,
+    UserResultComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GithubUsersServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
